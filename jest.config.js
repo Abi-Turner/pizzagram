41488ab989c10b9f2b5344fb0dea1a8663ecbd59
Abi Turner <turner.abi@gmail.com>
2024-06-24 10:47:53 +0100

    updated files  from typescript to js and updated config to reflect. removed store folder until needed.

--- jest.config.js
+++ jest.config.js
@@ -1,12 +1,4 @@
-// jest.config.js
-module.exports = {
-  preset: "ts-jest",
-  testEnvironment: "jsdom",
-  moduleNameMapper: {
-    "^@/(.*)$": "<rootDir>/src/$1", // Alias for src folder
-    "\\.css$": "<rootDir>/src/__mocks__/styleMock.js", // Mock CSS imports
-  },
-  setupFilesAfterEnv: ["<rootDir>/src/setupTests.ts"], // Setup file for Jest
-  transformIgnorePatterns: ["/node_modules/"], // Ignore transformation for node_modules
-  moduleFileExtensions: ["ts", "tsx", "js", "jsx", "json", "node"],
-};
+export const testEnvironment = "jsdom";
+export const moduleFileExtensions = ["js", "jsx"];
+export const setupFilesAfterEnv = ["<rootDir>/setupTests.js"];
+export const testMatch = ["<rootDir>/src/**/*.{spec,test}.{js,jsx}"];
--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,16 @@
+import React from "react";
 import Login from "./components/Login";
 import Feed from "./components/Feed";
 import Profile from "./components/Profile";
 
 function App() {
-    return (
-        <>
-            <Login />
-            <Feed />
-            <Profile />
-        </>
-    )
+  return (
+    <>
+      <Login />
+      <Feed />
+      <Profile />
+    </>
+  );
 }
 
 export default App;
